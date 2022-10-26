29270e47936fa4b960d6409518a3ed3cf1e32b13
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-26 17:57:53 +0200

    add: for statement per la creazione dei div

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//creazione array di img da aggiungere nel carosello
+const imgArray=[ '/img/01.jpg', '/img/02.jpg', '/img/03.jpg', '/img/04.jpg', '/img/05.jpg']
+//variabili
+const itemContainer= document.querySelector("div.items")
+
+//for statement per concatenare le img nei div
+for (let i = 0; i < imgArray.length; i++) {
+    const item= `<div class="item">
+                 <img src="${imgArray[i]}" alt="">
+                 </div>`
+    itemContainer.innerHTML += item //aggiungo i div descritti sopra all'interno del div container definito a inizio documento
+}
